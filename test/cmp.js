9c0e00e083ec7f803b646d1c53eba1ad6feebfc6
Oussama Ezzaou <oezzaou@c2r1p9.1337.ma>
2025-01-21 07:30:56 +0100

    js: Numbers in javascript

---
+++ test/cmp.js
@@ -0,0 +1,5 @@
+nbr1 = 10;
+nbr2 = '10';
+
+console.log("nbr1 == nbr2 = " + (nbr1 == nbr2));
+console.log("nbr1 === nbr2 = " + (nbr1 === nbr2));
---
+++ test/var.js
@@ -0,0 +1,8 @@
+height = 20
+width = 10
+let randVar;
+
+console.log(" 5 ** 2 = " + (5 ** 2));
+console.log("let = " + typeof randVar);
+console.log("area = " + typeof height);
+console.log("20 is String = ", 20 instanceof String);
