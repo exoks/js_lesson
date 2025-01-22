362f574aaf16772039d6d06b557c2a40d79d6789
Oussama Ezzaou <oezzaou@c2r1p9.1337.ma>
2025-01-22 19:04:25 +0100

    js: dom manipulation

--- test/dom.js
+++ test/dom.js
@@ -1,10 +1,57 @@
+//  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣤⣦⣴⣶⣾⣿⣶⣶⣶⣶⣦⣤⣄⠀⠀⠀⠀⠀⠀⠀                                              
+//  ⠀⠀⠀⠀⠀⠀⠀⢠⡶⠻⠛⠟⠋⠉⠀⠈⠤⠴⠶⠶⢾⣿⣿⣿⣷⣦⠄⠀⠀⠀                  𓐓  dom.js 𓐔           
+//  ⠀⠀⠀⠀⠀⢀⠔⠋⠀⠀⠤⠒⠒⢲⠀⠀⠀⢀⣠⣤⣤⣬⣽⣿⣿⣿⣷⣄⠀⠀                                              
+//  ⠀⠀⠀⣀⣎⢤⣶⣾⠅⠀⠀⢀⡤⠏⠀⠀⠀⠠⣄⣈⡙⠻⢿⣿⣿⣿⣿⣿⣦⠀   Student: oezzaou <oezzaou@student.1337.ma> 
+//  ⢀⠔⠉⠀⠊⠿⠿⣿⠂⠠⠢⣤⠤⣤⣼⣿⣶⣶⣤⣝⣻⣷⣦⣍⡻⣿⣿⣿⣿⡀                                              
+//  ⢾⣾⣆⣤⣤⣄⡀⠀⠀⠀⠀⠀⠀⠀⠉⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇                                              
+//  ⠀⠈⢋⢹⠋⠉⠙⢦⠀⠀⠀⠀⠀⠀⢀⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇       Created: 2025/01/22 10:57:13 by oezzaou
+//  ⠀⠀⠀⠑⠀⠀⠀⠈⡇⠀⠀⠀⠀⣠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠇       Updated: 2025/01/22 15:33:01 by oezzaou
+//  ⠀⠀⠀⠀⠀⠀⠀⠀⡇⠀⠀⢀⣾⣿⣿⠿⠟⠛⠋⠛⢿⣿⣿⠻⣿⣿⣿⣿⡿⠀                                              
+//  ⠀⠀⠀⠀⠀⠀⠀⢀⠇⠀⢠⣿⣟⣭⣤⣶⣦⣄⡀⠀⠀⠈⠻⠀⠘⣿⣿⣿⠇⠀                                              
+//  ⠀⠀⠀⠀⠀⠱⠤⠊⠀⢀⣿⡿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠘⣿⠏⠀⠀                             𓆩♕𓆪      
+//  ⠀⠀⠀⠀⠀⡄⠀⠀⠀⠘⢧⡀⠀⠀⠸⣿⣿⣿⠟⠀⠀⠀⠀⠀⠀⠐⠋⠀⠀⠀                     𓄂 oussama ezzaou𓆃  
+//  ⠀⠀⠀⠀⠀⠘⠄⣀⡀⠸⠓⠀⠀⠀⠠⠟⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀                                              
+
 
 var para = document.createElement('p');
 var content = document.createTextNode("Hello, world!");
 
 para.appendChild(content);
 
-var body = document.getElementsByTagName('body')[0]; 
+// var body = document.getElementById('bdy');
+var body = document.body;
 
-body.appendChild(body);
+body.appendChild(content);
 
+// change body content
+body.innerHTML = "<p> this is the changed paragraph </p>";
+
+// var div = document.createElement('div');
+// var cnt = document.createTextNode('new division');
+// div.appendChild(cnt);
+
+body.insertAdjacentHTML('beforebegin', "<div id='dv'> new division </div>");
+
+// remove div element
+var dv = document.getElementById('dv');
+
+// set an attribute id
+para.setAttribute('id', 'p');
+
+// get an attribute id
+console.log("id = " + para.getAttribute('id'));
+
+// remove an attribute id
+para.removeAttribute('id');
+
+console.log("id = " + para.getAttribute('id'));
+
+// set new id
+para.id = 'new id'
+
+// display the new id
+console.log("id = " + para.getAttribute('id'));
+
+para = document.getElementById('new id');
+
+dv.remove()
